71eb831d7e1045683f279d9d4db9aaa26afb7b53
Erick Patrick <erickckpatrick17@gmail.com>
2025-02-21 19:02:45 -0300

    chore: link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <h1>Bem-vindo ao Restaurante!</h1>
+      <Link href="/fsw-donalds">
+        <a>Ir para o menu do Restaurante</a>
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
